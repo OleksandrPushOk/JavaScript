a9b9ddb1bd3a4add09e6c9fa72eba4c34693ecc4
OleksandrPushOk <koshup90@gmail.com>
2021-06-27 22:33:31 +0300

    Записав та закоментував задачі з бота по Модулю 2

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+// MODULES
 import './js/module-1-0.js'
 // import './js/module-1-1.js'
 // import './js/module-1-2.js'
+//  import './js/module-2-1.js'
+//   import './js/module-2-2.js'
+
+// TASKS
+// import './tasks/module-1.js'
+// import './tasks/module-2.js'
 
--- js/module-1-0.js
+++ js/module-1-0.js
@@ -363,4 +363,359 @@
 // }
 
 
+                        // Модуль 2 //
+
+// function checkAge(age) {
+//   if (age>=18) { // Change this line
+//     return "You are an adult";
+//   }
+
+//   return "You are a minor";
+// }
+
+
+
+
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+//   // Change code below this line
+
+
+//   if (password === ADMIN_PASSWORD) {
+//     return "Welcome!";
+//   }
+
+//   return "Access denied, wrong password!";
+//   // Change code above this line
+// }
+
+
+
+//№3
+
+// function checkStorage(available, ordered) {
+
+//   if (ordered === 0) {
+//     return "Your order is empty!";
+    
+//   }if (ordered > available) {
+//     return "Your order is too large, not enough goods in stock!";
+//   } 
+//     return "The order is accepted, our manager will contact you";
+
+// }
+
+
+
+
+// const fruits = ["apple", "plum", "pear", "orange"]
+
+
+
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// const firstElement = fruits[0]
+// const secondElement = fruits[1]
+// const lastElement = fruits[3]
+
+
+
+
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// fruits[1] = "peach";
+// fruits[3] = "banana";
+
+
+
+
+// const fruits = ["apple", "peach", "pear", "banana"];
+// const fruitsArrayLength = fruits.length;
+
+
+
+
+// const fruits = ["apple", "peach", "pear", "banana"];
+// const lastElementIndex = fruits.length - 1;
+// const lastElement = fruits[lastElementIndex];
+
+
+
+
+// function getExtremeElements(array) {
+//   const firstItem = array[0];
+//   const lastItem = array[array.length-1];
+//   const newarray = [firstItem, lastItem];
+//   return newarray ;  
+// }
+
+
+
+
+// function splitMessage(message, delimeter) {
+//   let words;
+// words = message.split(delimeter); 
+//   return words;
+// }
+
+
+
+
+// function calculateEngravingPrice(message, pricePerWord) {
+// const sms = message.split (' ');
+//   const price = sms.length * pricePerWord;
+// return price;
+// }
+
+
+
+
+// function makeStringFromArray(array, delimeter) {
+//   let string;
+//  string = array.join(delimeter); 
+//   return string;
+// }
+
+
+
+
+// function slugify(title) {
+// let lowerString = title.toLowerCase()
+// let string = lowerString.split(" ")
+// let slug = string.join("-");
+// return slug;
+// }
+
+
+
+
+// const fruits = ['apple', 'plum', 'pear', 'orange', 'banana'];
+// const firstTwoEls = fruits.slice(0, 2);
+// const nonExtremeEls = fruits.slice(1, 4);
+// const lastThreeEls = fruits.slice(-3);
+
+
+
+
+// №15
+
+// const oldClients = ['Mango', 'Ajax', 'Poly', 'Kiwi'];
+// const newClients = ['Peach', 'Houston'];
+// const allClients = oldClients.concat(newClients); 
+
+
+
+
+// function makeArray(firstArray, secondArray, maxLength) {
+//   const takeArray = firstArray.concat(secondArray).slice(0, maxLength);
+//   return takeArray
+//  }
+
+
+
+
+// const start = 3;
+// const end = 7;
+// for (let i = start; i <= end; i += 1 ) { // Change this line
+//   console.log(i);
+// }
+
+
+
+
+// function calculateTotal(number) { 
+// let result = 0
+// for (let i = 0; i <= number; i += 1 )
+// result += i
+//   { 
+//   return result
+//   console.log(i);
+// }
+// }
+
+
+
+
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+// for (let i = 0; i < fruits.length; i += 1) {
+//   const fruit = fruits[i]; 
+//   console.log(fruit);
+// }
+
+
+
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   for (let i = 0; i < order.length; i += 1){
+//    total += order[i];
+//   }
+//   return total;
+// }
+
+
+
+
+// function findLongestWord(string) {
+//   let longestWord = "";
+//   const redstring = string.split(" ");
+//   for(let i = 0; i < redstring.length; i += 1){
+//     if (redstring[i].length > longestWord.length){
+//       longestWord = redstring[i];
+//     }
+//   } 
+//   return longestWord; 
+// }
+
+
+
+
+// function createArrayOfNumbers(min, max) {
+ 
+//   const numbers = [];
+//   for(let i = min; i <= max; i +=1)
+//    numbers.push(i) 
+//   return numbers;
+// }
+
+
+
+
+// function filterArray(numbers, value) {
+//   const num = [];
+//   for(let i = 0; i <= numbers.length; i += 1){
+//     if (numbers[i] > value) {
+//     num.push(numbers[i]);
+//      }
+//   }
+//   return num;
+// }
+
+
+
+
+// function checkFruit(fruit) {
+//   const fruits = ["apple", "plum", "pear", "orange"]; 
+//   return fruits.includes(fruit);
+// }
+  
+
+
+
+// function getCommonElements(array1, array2) {
+//   const seeArray = [];
+//   for (let i = 0; i < array1.length; i+= 1) {
+//     if (array2.includes(array1[i])) {
+//     seeArray.push(array1[i]);  
+//     }
+//   }
+//   return seeArray;
+// }
+// console.log(getCommonElements([1, 2, 3], [2, 4]));
+// console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
+// console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+// console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
+// console.log(getCommonElements([1, 2, 3], [10, 20, 30]));
+  
+
+
+
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//    for (let value of order) {
+//    total += value
+//   }
+//   return total;
+// }
+
+
+
+
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
+//   for (const number of numbers) {  
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   }
+//   return filteredNumbers;
+// }
+
+
+
+
+// const a = 3 % 3;
+// const b = 4 % 3;
+// const c = 11 % 8;
+// const d = 12 % 7;
+// const e = 8 % 3;
+
+
+
+
+// function getEvenNumbers(start, end) {
+//   const see = [];
+//   for (let i = start; i <= end; i += 1) {
+//    if (i % 2 === 0) { 
+//      see.push (i)} 
+//   } 
+//   return see;
+//   }
+// console.log(getEvenNumbers (2, 5));
+// console.log(getEvenNumbers (3, 11));
+// console.log(getEvenNumbers (6, 12));
+// console.log(getEvenNumbers (8, 8));
+// console.log(getEvenNumbers (7, 7));
+
+
+
+
+// const start = 6;
+// const end = 27;
+// let number;
+// for (let i = start; i < end; i += 1) {
+//   if (i % 5 === 0) {
+//     number = i;
+//     break
+//   }
+// }
+
+
+
+
+// function findNumber(start, end, divisor) {
+//   let number;
+//   for (let i = start; i < end; i += 1) {
+//     if (i % divisor === 0) {
+//       number = i;
+//       return i;
+//     }
+//   }
+//   return number;
+// }
+
+
+
+
+// function includes(array, value) {
+//   for (let see of array) {
+//     if (see === value)
+//       return true;
+//   }
+//   return false; 
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
