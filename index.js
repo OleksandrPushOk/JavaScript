1035608698cdb461ccd265d8aef0bcb8a42e7121
OleksandrPushOk <koshup90@gmail.com>
2021-07-04 22:26:01 +0300

    Закинув навчальні матеріали Модулю 3

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ import './js/module-1-0.js'
 // import './js/module-1-2.js'
 //  import './js/module-2-1.js'
 //   import './js/module-2-2.js'
+//   import './js/module-3-1-spread_rest.js'
+// import './js/module-3-1-destructuring.js'
+//   import './js/module-3-1-objects.js'
 
 // TASKS
 // import './tasks/module-1.js'
 // import './tasks/module-2.js'
+// import './tasks/module-3.js'
 
